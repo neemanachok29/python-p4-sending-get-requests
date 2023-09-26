5b8eb3d9aa71340a2476994d12390c51e0c44958
Neema Nachok <neemanachok29@gmail.com>
2023-09-26 17:22:24 +0300

    done

--- lib/get.py
+++ lib/get.py
@@ -1,10 +1,4 @@
+#aLL THIS IS RUN IN THE REPL 
+
 import requests
 import json
-
-url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
-
-response = requests.get(url)
-
-json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
